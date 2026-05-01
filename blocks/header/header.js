6a9d04f4db5589bb9a8d7db93e284fd5e2e54f35
Ankan Ghosh <ankan.ghosh@axeno.co>
2026-05-01 17:37:40 +0530

    feat: implement header block with dynamic navigation parsing and responsive submenu support

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -293,7 +293,7 @@ export default async function decorate(block) {
   hamburgerButton.setAttribute('aria-label', 'Open navigation');
   hamburgerButton.innerHTML = `
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
-      <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5 0 0 1 0 1H3a.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5 0 0 1 0 1H3a.5 0 0 1-.5-.5z"></path>
+      <path fill-rule="evenodd" d="M2.5 12a.5 .5 0 0 1 .5 -.5h10a.5 .5 0 0 1 0 1H3a.5 .5 0 0 1 -.5 -.5zm0 -4a.5 .5 0 0 1 .5 -.5h10a.5 .5 0 0 1 0 1H3a.5 .5 0 0 1 -.5 -.5zm0 -4a.5 .5 0 0 1 .5 -.5h10a.5 .5 0 0 1 0 1H3a.5 .5 0 0 1 -.5 -.5z"></path>
     </svg>
   `;
   hamburgerButton.addEventListener('click', () => toggleMenu(navInstance, navSectionsContainer));
@@ -317,3 +317,4 @@ export default async function decorate(block) {
   toggleMenu(navInstance, navSectionsContainer, isDesktop.matches);
   isDesktop.addEventListener('change', () => toggleMenu(navInstance, navSectionsContainer, isDesktop.matches));
 }
+
